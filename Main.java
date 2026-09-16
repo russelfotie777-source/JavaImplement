bb63548a67362c1121d8b798eefd43eb12baf745
Ton Nom <tonemail@example.com>
2026-09-16 22:41:29 +0200

    Connection du Backend au new interface et amelioration du front

--- Main.java
+++ Main.java
@@ -4,21 +4,49 @@
 
         public static void main(String[] args)
     {
-        CompteBancaire compteBancaire1 = new CompteBancaire("paul",50000);
+        //création des objets CompteBancaire
+        CompteBancaire compteBancaire1 = new CompteBancaire("paul",-50000);
         CompteBancaire compteBancaire2 = new CompteBancaire("Marie",120000.0);
-        System.out.println(compteBancaire1.getSolde());
-        compteBancaire1.deposer(100000.0);
-        compteBancaire1.deposer(-10000.0);
+        Client client1 = new Client("paul", "12345678");
+        //depot d'argent sur le compteBancaire1
+        boolean resultatDepot = compteBancaire1.deposer(100000.0);
+          if(resultatDepot)
+          {
+             System.out.println("Depot effectué avec Succes");
+          }else{
+             System.out.println("Depot échoué");
+          }
+        //retait d'argent sur le compteBancaire1
+        boolean resultatRetrait1 = compteBancaire1.retirer(20000.0);
+        boolean resultatRetrait2 = compteBancaire1.retirer(200000.0);
+
+        //impressions
+        CompteBancaire.afficherNomBanque();
+        compteBancaire1.afficherPropriétaire();
+        client1.afficherClient();
         System.out.println(compteBancaire1.getTitulaire());
+        System.out.println(compteBancaire1.getSolde());
+        if(resultatRetrait1)
+        {
+            System.out.println("Retrait effectué avec Succes");
+        }else{
+            System.out.println("Retrait échoué");
+        }
+        if(resultatRetrait2)
+        {
+            System.out.println("retrait effectué avec succes");
+        }else{
+            System.out.println("retrait échoué");
+        }
         System.out.println(compteBancaire2.getTitulaire());
         System.out.println(compteBancaire2.getSolde());
         System.out.println(compteBancaire1.getSolde());
-        compteBancaire1.retirer(30000.0);
-        System.out.println("Le nouveau Solde est de "+compteBancaire1.getSolde());
-
-        //teste de l'enseignant
+        System.out.println("Le nouveau Solde est de " + compteBancaire1.getSolde());
         System.out.println(compteBancaire1.getSolde());
-        
+        System.out.println("Le nombre de comptes créés est de : "+CompteBancaire.getNombreDeComptes());
+
+
+
         
 
     }
